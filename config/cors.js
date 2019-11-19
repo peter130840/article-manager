8cdc79f57edafcdada2894a49903c81d590b4505
Peter <peter130840@hotmail.com>
2019-11-20 04:23:26 +0800

    First commit and barebone

---
+++ config/cors.js
@@ -0,0 +1,10 @@
+let cors = require('cors')
+
+const cors_define = cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+    credentials: true,
+})
+
+module.exports = cors_define
---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('path')
+require('dotenv').config({ path: './config/dev.env' })
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+const articleRouter = require('./routes/articles')
+const cors_define = require('../config/cors')
+const error = require('./middlewares/errHandler')
+
+class app {
+    constructor() {
+        this.app = express()
+        this.middleware()
+        this.routes()
+        this.error()
+    }
+    middleware() {
+        this.app.use(express.json()) //Don't need bodyParser
+        this.app.use(express.urlencoded({ extended: false }))
+        this.app.use(cors_define)
+    }
+
+    routes() {
+        this.app.use('/', indexRouter)
+        this.app.use('/users', usersRouter)
+        this.app.use('/articles', articleRouter)
+    }
+    error() {
+        this.app.use(error.errorHandler)
+    }
+}
+
+module.exports = new app().app
